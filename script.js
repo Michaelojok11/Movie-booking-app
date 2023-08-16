8e339060531fd6db8539cdbd0e2d0ebe7cb1a606
Michaelojok11 <michaelocan11@gmail.com>
2023-08-16 20:06:35 +0300

    update git

--- script.js
+++ script.js
@@ -4,12 +4,47 @@ const count = document.getElementsByName('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// get data from local storage and populate the ui
+function populationUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+    if (selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) =>{
+            if(selectedSeats.indexOf(index) > -1)
+            seat.classList.add('selected');
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex != null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 let ticketPrice = +movieSelect.value; 
+
+// save selected movie  and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
+
+
+
 
 // update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
     
+    // update local storage
+    const seatsIndex = [...selectedSeats].map(function(seat){
+        return [...seats].indexOf(seat);
+    });
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
     //    Get the length of the selected seats
     const selectedSeatsCount = selectedSeats.length;
     
@@ -19,14 +54,12 @@ function updateSelectedCount(){
 }
 
 // movie select event
-movieSelect.addEventListener('change', e => {
+    movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    
     updateSelectedCount();
-})
-
-
-
-
+});
 
 
 
@@ -41,4 +74,7 @@ container.addEventListener('click', (e) => {
     // update function to update the selected counts and price
     updateSelectedCount();
 });
+
+updateSelectedCount();
+
 
